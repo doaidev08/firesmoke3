ca176dbebf897e6b4037ad9936b9750b5068f349
doaidev08 <89960196+doaidev08@users.noreply.github.com>
2022-03-19 12:36:42 +0700

    Update webcamtest.py

--- webcamtest.py
+++ webcamtest.py
@@ -1,6 +1,6 @@
 import cv2
 import streamlit as st
-from PIL import Image
+
 st.title("Webcam Live Feed")
 run = st.checkbox('Run')
 FRAME_WINDOW = st.image([])
@@ -8,6 +8,7 @@ camera = cv2.VideoCapture(0)
 
 while run:
     _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     FRAME_WINDOW.image(frame)
 else:
     st.write('Stopped')
